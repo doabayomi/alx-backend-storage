6762d46343e863b43ace4cb702d189f28f5b8c32
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-17 23:14:39 +0000

    Redoing 3 and 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,11 @@
 --Trigger to ret attribute valid_email when email has been changed
-DROP TRIGGER IF EXISTS validate_email;
-DELIMITER $$
-CREATE TRIGGER validate_mail_on_change
-BEFORE UPDATE ON users
-FOR EACH ROW
-BEGIN
-    IF OLD.email != NEW.email THEN
-        SET NEW.valid_email = 0;
-    ELSE
-        SET NEW.valid_email = NEW.valid_email;
-    END IF;
-END $$
-DELIMITER ;
+delimiter |
+
+CREATE TRIGGER valid_email BEFORE UPDATE ON users
+       FOR EACH ROW
+       BEGIN
+           IF NEW.email != OLD.email THEN
+               SET NEW.valid_email = 0;
+           END IF;
+       END
+|
