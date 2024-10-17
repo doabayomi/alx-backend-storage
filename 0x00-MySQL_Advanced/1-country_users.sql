cfb8d5704604d296b16cee173d8f34e2866fdb52
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-17 23:08:56 +0000

    Checking 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 -- Creating a table of users with country options
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    country CHAR(2) NOT NULL DEFAULT 'US' CHECK(country IN ('US', 'CO', 'TN'))
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
