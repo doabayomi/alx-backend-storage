cbb29e8b198b2c8cdc910498e7accf211f5963cd
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-21 15:04:14 -0700

    Fixed 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""Stats finder for logs"""
 from pymongo import MongoClient
 
 """Finding total logs"""
-client = MongoClient('mongodb://127.0.0.1:27017')
+client = MongoClient('mongodb://localhost:27017')
 nginx_logs = client.logs.nginx
 total_logs = nginx_logs.count_documents({})
 print(f"{total_logs} logs")
@@ -11,10 +12,11 @@ print(f"{total_logs} logs")
 print("Methods:")
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for method in methods:
-    method_count = nginx_logs.count_documents({"\tmethod": method})
-    print(f"method {method}: {method_count}")
+    method_count = nginx_logs.count_documents({"method": method})
+    print(f"\tmethod {method}: {method_count}")
 
 """Finding total status checks"""
 status_checks = nginx_logs.count_documents({"method": "GET",
                                             "path": "/status"})
 print(f"{status_checks} status check")
+
