3ba04fe88c407258d6d33d6d82e5676790565425
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-22 04:44:47 -0700

    Trying a new main

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,10 +2,13 @@
 """Stats finder for logs"""
 from pymongo import MongoClient
 
-if __name__ == '__main__':
-    """Finding total logs"""
+
+def main():
+    """Printing stats"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_logs = client.logs.nginx
+
+    """Finding total logs"""
     total_logs = nginx_logs.count_documents({})
     print(f"{total_logs} logs")
 
@@ -22,3 +25,7 @@ if __name__ == '__main__':
     print(f"{status_checks} status check")
 
     client.close()
+
+
+if __name__ == '__main__':
+    main()
